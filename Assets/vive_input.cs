4812ae56097af03143e3be1f22fcb8e8aaf23966
Stazia Tronboll <WigginWannabe@users.noreply.github.com>
2019-02-08 13:49:41 -0800

    New repo for demo code

---
+++ Assets/vive_input.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class vive_input : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(SteamVR_Input._default.inActions.Teleport.GetStateDown(SteamVR_Input_Sources.Any)) {
+		print("Teleport down");
+	}
+    }
+}
